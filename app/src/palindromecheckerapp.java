6a8be3378c98e86edf6db065364a68fa3d95d0e7
haridheep10 <haridheep10@gmail.com>
2026-02-27 16:24:32 +0530

    uc2-HardcodedPalindrome

--- app/src/palindromecheckerapp.java
+++ app/src/palindromecheckerapp.java
@@ -1,7 +1,25 @@
-public class palindromecheckerapp {
+import java.util.Scanner;
+public class PalindromeCheckerApp {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Palindrome Checker Management System");
-        System.out.println("Version : 1.0");
-        System.out.println("System initialized successfully.");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Input text: ");
+        String str = sc.nextLine();
+
+        String reversed = "";
+
+        // Reverse the string
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversed += str.charAt(i);
+        }
+
+        // Check palindrome
+        if (str.equalsIgnoreCase(reversed)) {
+            System.out.println("Is it a Palindrome? : true");
+        } else {
+            System.out.println("Is it a Palindrome? : false");
+        }
+
+        sc.close();
     }
 }
